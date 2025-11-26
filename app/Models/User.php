244fa86253d26f1b2e1e12dce61d92be4fa6bf68
Yuri <reyesyuridiaselene@gmail.com>
2025-11-25 23:31:46 -0600

    yolo

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,12 +6,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Laravel\Sanctum\HasApiTokens;//maneja los tokens por me daba error
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;//hace los tokes parte de la clase user para que no me de error 
     use HasFactory, Notifiable;
 
     /**
