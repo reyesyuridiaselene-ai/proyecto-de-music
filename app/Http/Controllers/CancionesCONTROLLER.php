66410cd6774b89a7aae7c56ae38c821b5168adad
Yuri <reyesyuridiaselene@gmail.com>
2025-12-01 03:32:26 -0600

    todo funciona al 100

--- app/Http/Controllers/CancionesCONTROLLER.php
+++ app/Http/Controllers/CancionesCONTROLLER.php
@@ -3,35 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\canciones;//usamos el use para que sea una herramienta de legibilidad
+use App\Models\Canciones;
 
 class CancionesController extends Controller
 {
-    //
-    
-    public function nuevaCancion(Request $request){
-        //poner la parte de use cancion para 
-       // $cancion = Cancion::create($request->all());
+    public function nuevaCancion(Request $request)
+    {
         $cancion = Canciones::create([
-        'nombre' => $request->nombre, 
-        'artista' => $request->artista,
-        'tiempo' => $request->tiempo,
-        'genero_id' => $request->genero_id
+            'nombre' => $request->nombre, 
+            'artista' => $request->artista,
+            'tiempo' => $request->tiempo,
+            'genero_id' => $request->genero_id
         ]);
 
         return response()->json(['message' => 'Canción creada', 'id' => $cancion->id], 201);
-           
     }
 
     public function index()
     {
-        $canciones = \App\Models\Canciones::with('genero')->get();
-
-        // Devuelve el array de canciones en formato JSON con código 200 (OK)
+        $canciones = Canciones::with('genero')->get();
         return response()->json($canciones, 200);
     }
-
-    // App\Http\Controllers\CancionesController.php
 
     public function destroy($id)
     {
@@ -39,24 +31,20 @@ class CancionesController extends Controller
         if (!$cancion) {
             return response()->json(['message' => 'Canción no encontrada'], 404);
         }
+        
         $cancion->delete();
         return response()->json(['message' => 'Canción eliminada con éxito'], 200);
     }
 
-    // App\Http\Controllers\CancionesController.php
-
     public function update(Request $request, $id)
     {
-        $cancion = \App\Models\Canciones::find($id);
+        $cancion = Canciones::find($id);
 
         if (!$cancion) {
             return response()->json(['message' => 'Canción no encontrada'], 404);
         }
 
-        // 3. Aplicar los nuevos datos de la solicitud al objeto de la canción.
-        // fill() toma todos los datos del $request (si están en $fillable)
         $cancion->fill($request->all());
-        
         $cancion->save();
 
         return response()->json($cancion, 200);
--- app/Models/Canciones.php
+++ app/Models/Canciones.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Canciones extends Model
 {
-    //protected $table = 'cancion';
+    
     protected $table = 'cancion';
     protected $fillable = ['nombre', 'artista', 'tiempo', 'genero_id'];
     protected $guarded = [];
